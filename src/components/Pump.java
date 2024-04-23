5d4c6b14af54f7e396243eebc85936c0f10de906
Anastasiia Mandrychenko <nastyamarichenko@gmail.com>
2024-04-23 19:42:50 +0200

    added methods to the pump class

--- src/components/Pump.java
+++ src/components/Pump.java
@@ -1,5 +1,47 @@
 package components;
 
-public class Pump {
-    
+import java.util.UUID;
+
+import interfaces.ILeakage;
+
+public class Pump extends Component implements ILeakage{
+    private int connectablePipesNumber;
+    private boolean isReservoirFull;
+    private long leakStartTime;
+    private boolean isLeaking;
+    private Pipe incomingPipe;
+    private Pipe outgoingPipe;
+    private boolean isBroken;
+
+    public Pump(UUID componentID, int connectablePipesNumber) {
+        super(componentID);
+        this.connectablePipesNumber = connectablePipesNumber;
+    }
+    public void connectPipe(Pipe newPipe) {
+        if (incomingPipe == null) {
+            incomingPipe = newPipe;
+        } else if (outgoingPipe == null) {
+            outgoingPipe = newPipe;
+        }
+    }
+    public void fillReservoir() {
+        if (!isReservoirFull) {
+            isReservoirFull = true;
+        }
+    }
+    public void startLeaking() {
+        if (isReservoirFull) {
+            isLeaking = true;
+            leakStartTime = System.currentTimeMillis();
+        }
+    }
+    public long stopLeaking() {
+        if (isLeaking) {
+            isLeaking = false;
+            long leakDuration = System.currentTimeMillis() - leakStartTime;
+            leakStartTime = 0; // Reset leak start time
+            return leakDuration;
+        }
+        return 0; 
+    }
 }
