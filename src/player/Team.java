cf8ce366be0a5b831e1627e63f2e85c0f9f0b6e1
Mihaela Melnic <moldovamelnik@gmail.com>
2024-04-28 19:05:31 +0200

    Added JavaDoc comments to Team

--- src/player/Team.java
+++ src/player/Team.java
@@ -5,15 +5,31 @@ import java.util.List;
 
 import interfaces.IScorer;
 
+/**
+ * The Team class represents a group of movable players within the game.
+ * It manages the assignment of players to the team and provides access to the
+ * team's score.
+ */
 public class Team {
-    private List<MovablePlayer> players;
-    private IScorer teamScore;
+    private List<MovablePlayer> players; // The list of players in the team
+    private IScorer teamScore; // The scoring mechanism for the team
 
+    /**
+     * Constructs a new Team object with the specified scoring mechanism.
+     *
+     * @param teamScore The scoring mechanism for the team.
+     */
     public Team(IScorer teamScore) {
         this.players = new ArrayList<>();
         this.teamScore = teamScore;
     }
 
+    /**
+     * Assigns a player to the team.
+     * Adds the specified player to the team's roster.
+     *
+     * @param player The player to be assigned to the team.
+     */
     public void assignPlayer(MovablePlayer player) {
         if (player != null) {
             players.add(player);
@@ -23,17 +39,29 @@ public class Team {
         }
     }
 
-    // Getter for players
+    /**
+     * Retrieves the list of players in the team.
+     *
+     * @return The list of players in the team.
+     */
     public List<MovablePlayer> getPlayers() {
         return players;
     }
 
-    // Setter for teamScore
+    /**
+     * Sets the scoring mechanism for the team.
+     *
+     * @param teamScore The scoring mechanism to be set for the team.
+     */
     public void setTeamScore(IScorer teamScore) {
         this.teamScore = teamScore;
     }
 
-    // Getter for teamScore
+    /**
+     * Retrieves the scoring mechanism for the team.
+     *
+     * @return The scoring mechanism for the team.
+     */
     public IScorer getTeamScore() {
         return teamScore;
     }
