9097c73356c9451b7dec7300e9e0d5d5cd238747
Anastasiia Mandrychenko <nastyamarichenko@gmail.com>
2024-04-28 21:08:01 +0200

    added javadoc comments

--- src/interfaces/ILeakage.java
+++ src/interfaces/ILeakage.java
@@ -1,8 +1,20 @@
 package interfaces;
 
+/**
+ * The {@code ILeakage} interface represents objects that have the ability to start and stop leaking.
+ * Implementing classes should provide functionality to start and stop leakage.
+ */
 public interface ILeakage {
-    void startLeaking();
     
+    /**
+     * Starts the leakage.
+     * Implementing classes should define the behavior of starting the leakage.
+     */
+    void startLeaking();
+    /**
+     * Stops the leakage.
+     * Implementing classes should define the behavior of stopping the leakage.
+     */
     void stopLeaking();
     
 }
