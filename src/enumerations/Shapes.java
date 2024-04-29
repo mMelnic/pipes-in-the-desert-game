ec964d64fd629d987180a1dffcf5010100fe599f
Buyanjargal Byambajav <okoo1129@gmail.com>
2024-04-29 02:37:21 +0200

    comments and some change in cistern class

--- src/enumerations/Shapes.java
+++ src/enumerations/Shapes.java
@@ -1,10 +1,17 @@
 package enumerations;
-
+/**
+ * the enumeration Shapes is responsible to define shape og a pipe
+ */
 public enum Shapes {
     HORIZONTAL,
     VERTICAL,
     CORNER_LEFT_UP,
     CORNER_RIGHT_UP,
     CORNER_LEFT_DOWN,
-    CORNER_RIGHT_DOWN
+    CORNER_RIGHT_DOWN,
+    T_UP,
+    T_DOWN,
+    T_RIGHT,
+    T_LEFT,
+    CROSS
 }
