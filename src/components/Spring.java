2f759aad1f093fe74b6c2539cbff79b7d29b9932
Mihaela Melnic <moldovamelnik@gmail.com>
2024-05-10 11:50:58 +0200

    Initial startWaterSupply spring

--- src/components/Spring.java
+++ src/components/Spring.java
@@ -1,5 +1,8 @@
 package components;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import system.Cell;
 
 /**
@@ -20,10 +23,32 @@ public class Spring extends Component
 	/**
 	 * a method that starts water supply from a spring which sets an attribute isWaterFlowing to true.
 	 */
-	public void startWaterSupply() 
-	{
-		isWaterFlowing = true;
-	}
+	public void startWaterSupply() {
+    // Initialize a set to keep track of visited components
+    Set<Component> visited = new HashSet<>();
+    // Call the recursive method to start water supply
+    startWaterSupplyDFS(this, visited);
+}
+
+// Recursive method to start water supply using depth-first search (DFS)
+private void startWaterSupplyDFS(Component component, Set<Component> visited) {
+    // Mark the current component as visited
+    visited.add(component);
+
+    // If the current component is a pipe, set its isWaterFlowing attribute to true
+    if (component instanceof Pipe) {
+        ((Pipe) component).setWaterFlowing(true);
+    }
+
+    // Traverse the connected components recursively
+    for (Component connectedComponent : component.getConnectedComponents().values()) {
+        // Skip if the connected component has already been visited
+        if (!visited.contains(connectedComponent) && (!(connectedComponent instanceof Pump) || (connectedComponent instanceof Pump))) { // TODO bred
+                startWaterSupplyDFS(connectedComponent, visited);
+            }
+    }
+}
+
 	/**
 	 * a method that is invoked when a pipe is connected to a spring, which calls the 
 	 * method startWaterSupply();
