e7f94b99de05340265e62d9f0f0e0383f343b2bf
Mihaela Melnic <moldovamelnik@gmail.com>
2024-05-10 11:11:22 +0200

    Exception pipe more than 2 connected components

---
+++ src/exceptions/PipeMultipleComponentsConnectedException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class PipeMultipleComponentsConnectedException extends Exception {
+    public PipeMultipleComponentsConnectedException(String string){
+        super(string);
+    }
+}
