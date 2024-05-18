0baf62cfe127bed7140007ed6c351a5c0e064491
Anastasiia Mandrychenko <nastyamarichenko@gmail.com>
2024-05-18 20:13:59 +0300

    added dropComponent, added connectPipeWithSpring/Cistern

--- src/GUI/PlumberController.java
+++ src/GUI/PlumberController.java
@@ -5,9 +5,15 @@ import java.awt.event.KeyEvent;
 
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import system.Map;
 
+import components.Cistern;
+import components.Component;
 import components.Pipe;
 import components.Pump;
+import components.Spring;
 import enumerations.Direction;
 import player.Plumber;
 import system.Cell;
@@ -16,12 +22,55 @@ public class PlumberController extends KeyAdapter {
     private Plumber plumberPlayer1;
     private PlumberView plumberView1;
     private JPanel mapPanel;
-
+    private Pipe selectedPipe = null;
+    private static final int CELL_SIZE = 80;
 
     public PlumberController(Plumber plumberPlayer1, JPanel mapPanel) {
         this.plumberPlayer1 = plumberPlayer1;
         this.plumberView1 = new PlumberView(plumberPlayer1);
         this.mapPanel = mapPanel;
+        mapPanel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                handleMousePressed(e);
+            }
+        });
+    }
+
+    private void handleMousePressed(MouseEvent e) {
+        int mouseX = e.getX();
+        int mouseY = e.getY();
+
+        int row = mouseY / CELL_SIZE;
+        int col = mouseX / CELL_SIZE;
+
+        Map map = plumberPlayer1.getCurrentCell().getMap();
+        Cell clickedCell = map.getCells(row, col);
+
+        if (clickedCell != null) {
+            Pipe clickedPipe = clickedCell.getComponent() instanceof Pipe ? (Pipe) clickedCell.getComponent() : null;
+            Component clickedComponent = clickedCell.getComponent();
+
+            if (clickedPipe != null || clickedComponent instanceof Spring || clickedComponent instanceof Cistern) {
+                if (selectedPipe == null) {
+                    // First click
+                    if (clickedPipe != null) {
+                        selectedPipe = clickedPipe;
+                    }
+                } else {
+                    // Second click
+                    if (clickedComponent instanceof Spring || clickedComponent instanceof Cistern) {
+                        plumberPlayer1.connectPipeWithComponent(selectedPipe, clickedComponent);
+
+                        selectedPipe = null;
+                    } else {
+                        selectedPipe = null;
+                    }
+                }
+            } else {
+                selectedPipe = null;
+            }
+        }
     }
 
     @Override
@@ -49,7 +98,9 @@ public class PlumberController extends KeyAdapter {
             case KeyEvent.VK_SLASH:
                 plumberPlayer1.installComponent(plumberPlayer1.getFacingDirection());
                 break;
-            // TODO: Add more key bindings 
+            case KeyEvent.	VK_COMMA:
+            plumberPlayer1.dropComponent();
+            
         }
         SwingUtilities.invokeLater(() -> mapPanel.repaint());
     }
