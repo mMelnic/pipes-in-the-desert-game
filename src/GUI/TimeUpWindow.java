3fff8fcfae02c60b11d7ade93db9bb27f0877b1d
Alexandre Kakhiani <kakhianisandro12@gmail.com>
2024-05-20 20:52:19 +0200

    added last javadocs

--- src/GUI/TimeUpWindow.java
+++ src/GUI/TimeUpWindow.java
@@ -8,15 +8,33 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * TimeUpWindow class provides a graphical user interface to display the final scores and the winner when the game time is up.
+ */
 public class TimeUpWindow {
     private JFrame frame;
     private GameManager gameManager;
 
+    /**
+     * Constructs a new TimeUpWindow.
+     *
+     * @param plumberScore the score of the Plumbers team
+     * @param saboteurScore the score of the Saboteurs team
+     * @param winner the winner of the game
+     * @param gameManager the GameManager instance that manages the game state
+     */
     public TimeUpWindow(int plumberScore, int saboteurScore, String winner, GameManager gameManager) {
         initialize(plumberScore, saboteurScore, winner);
         this.gameManager = gameManager;
     }
 
+    /**
+     * Initializes the contents of the frame.
+     *
+     * @param plumberScore the score of the Plumbers team
+     * @param saboteurScore the score of the Saboteurs team
+     * @param winner the winner of the game
+     */
     private void initialize(int plumberScore, int saboteurScore, String winner) {
         frame = new JFrame("Time's Up!");
         frame.setBounds(100, 100, 450, 300);
@@ -67,6 +85,9 @@ public class TimeUpWindow {
         frame.setResizable(false);
     }
 
+    /**
+     * Makes the frame visible.
+     */
     public void show(){
         frame.setVisible(true);
     }
